a773562aa4c15c829e35c7a8a462f6c7a8165f50
Mr WhiteKidd <austinslater58@gmail.com>
2022-08-21 19:19:20 -0500

    The ability to add a track to playlist is implemented new method to App.js -> add track by id and use of state binded value of this to the method addTrack in App,js onAdd is passed to search results and isRemoval is set to value of false down to Tracklist .addTrack method is passed to Track component new constructor to Track component and binded this.addTrack to the current value of this onClick event to the + button in Track.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -38,7 +38,18 @@ class App extends React.Component {
         },
       ],
     };
+    this.addTrack = this.addTrack.bind(this)
   }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.find((savedTrack) => savedTrack.id === track.id)) {
+      return;
+    }
+    tracks.push(track);
+    this.setState({ playlistTracks: tracks });
+  }
+
   render() {
     return (
       <div>
@@ -48,8 +59,12 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} />
-            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
+            <SearchResults searchResults={this.state.searchResults}
+            onAdd={this.addTrack} />
+            <Playlist
+              playlistName={this.state.playlistName}
+              playlistTracks={this.state.playlistTracks}
+            />
           </div>
         </div>
       </div>
--- src/components/TrackList/TrackList.js
+++ src/components/TrackList/TrackList.js
@@ -6,14 +6,15 @@ class TrackList extends React.Component {
   render() {
     return (
       <div className="TrackList">
-        {/* {
+        {
             this.props.tracks.map(track => {
                 return <Track track={track}
-                key={track.id}/>
+                key={track.id}
+                onAdd={this.props.onAdd} />
             })
-        } */}
+        }
       </div>
-    );
+    )
   }
 }
 
